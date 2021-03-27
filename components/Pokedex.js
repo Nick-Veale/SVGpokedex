647001ae803b45f0f53b747730b69680e962f954
NickVeale <novingle@gmail.com>
2021-03-28 01:35:29 +1300

    Just touching up some things

--- components/Pokedex.js
+++ components/Pokedex.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Pokedex.module.css";
 import { useSpring, animated } from "react-spring";
 
 export default function Pokedex(props) {
+  const [keyBoardFill, setKeyBoardFill] = useState(null);
+
+  const loadingProps = useSpring({
+    config: { mass: 1, tension: 80, friction: 1, clamp: true },
+    from: { opacity: 0 },
+    to: { opacity: props.loading ? 2 : 0 },
+  });
+  const oppositeProps = useSpring({
+    config: { mass: 1, tension: 80, friction: 1, clamp: true },
+    from: { opacity: 1 },
+    to: { opacity: props.loading ? 0 : 2 },
+  });
+
+  const handleKeyDown = () => {
+    const randomNum = Math.floor(Math.random() * 10);
+    setKeyBoardFill(randomNum);
+  };
+
+  console.log(props.abilities);
+
   return (
     <div className={styles.container}>
       <svg height={props.height} width={props.width} className={styles.pokedex}>
@@ -93,9 +113,16 @@ export default function Pokedex(props) {
         <g className={styles.pokedexEye}>
           <g stroke="black">
             <circle r="50" cx="60" cy="60" fill="white" />
-            <circle r="40" cx="60" cy="60" fill="rgb(178, 220, 235)" />
+            <circle r="40" cx="60" cy="60" fill="lightskyblue" />
+            <animated.circle
+              r="40"
+              cx="60"
+              cy="60"
+              fill="white"
+              style={loadingProps}
+            />
           </g>
-          <circle r="10" cx="50" cy="40" fill="rgb(185, 224, 245)" />
+          <circle r="10" cx="50" cy="40" fill="rgb(255,255,255,0.3)" />
         </g>
         <g className={styles.pokedexSmallEyes}>
           <circle
@@ -106,7 +133,7 @@ export default function Pokedex(props) {
             stroke="black"
             stroke-width="2"
           />
-          <circle r="5" cx="155" cy="32" fill="rgb(255, 187, 60)" />
+          <circle r="5" cx="155" cy="32" fill="rgb(255,255,255,0.3)" />
           <circle
             r="20"
             cx="210"
@@ -115,7 +142,7 @@ export default function Pokedex(props) {
             stroke="black"
             stroke-width="2"
           />
-          <circle r="5" cx="205" cy="32" fill="rgb(255, 255, 200)" />
+          <circle r="5" cx="205" cy="32" fill="rgb(255,255,255,0.3)" />
           <circle
             r="20"
             cx="260"
@@ -124,7 +151,7 @@ export default function Pokedex(props) {
             stroke="black"
             stroke-width="2"
           />
-          <circle r="5" cx="255" cy="32" fill="rgb(217, 255, 160)" />
+          <circle r="5" cx="255" cy="32" fill="rgb(255,255,255,0.3)" />
         </g>
         <g className="pokedexLeftDisplay">
           <g className="pokedexDisplay">
@@ -142,9 +169,15 @@ export default function Pokedex(props) {
               stroke="black"
               stroke-width="2"
             />
-            <image
+            <animated.path
+              d="M60 220 L 470 220 L 470 440 L 60 440 Z"
+              fill="white"
+              style={loadingProps}
+            />
+            <animated.image
               className={styles.image}
               href={props.imageUrl}
+              style={oppositeProps}
               alt=""
               x="60"
               y="220"
@@ -198,17 +231,24 @@ export default function Pokedex(props) {
               stroke="black"
               stroke-width="2"
             />
-            <foreignObject x="80" y="600" height="100" width="220">
-              <div className={styles.pokemonDetails}>
-                <div>
-                  Name: <u>{props.name}</u>
+            <animated.path
+              d="M80 600 h 220 v 100 h -220 v -100 z"
+              fill="white"
+              style={loadingProps}
+            />
+            {props.weight && (
+              <foreignObject x="80" y="600" height="100" width="220">
+                <div className={styles.pokemonDetails}>
+                  <div>
+                    Name: <u>{props.name}</u>
+                  </div>
+                  <br />
+                  <div>
+                    Type: <u>{props.type}</u>
+                  </div>
                 </div>
-                <br />
-                <div>
-                  Type: <u>{props.type}</u>
-                </div>
-              </div>
-            </foreignObject>
+              </foreignObject>
+            )}
           </g>
         </g>
         <g className="pokedexRightDisplay">
@@ -219,6 +259,96 @@ export default function Pokedex(props) {
             stroke="black"
             stroke-width="2"
           />
+          <animated.path
+            d="M 590 190 h 450 v 200 h -450 z"
+            fill="white"
+            style={loadingProps}
+          />
+          {props.weight && (
+            <foreignObject x="590" y="190" height="200" width="450">
+              <div className={styles.infoDisplay}>
+                <div className={styles.basicInfo}>
+                  <div>Height: {props.pokemonHeight}ft</div>
+                  <div>Weight: {props.weight}lb</div>
+                  <div>Number: {props.id}</div>
+                  <div>Abilities:</div>
+                  <ul>
+                    {props.abilities.map((item) => (
+                      <li>{item.ability.name}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className={styles.stats}>
+                  <div className={styles.stat}>
+                    <div className={styles.statName}>HP: </div>
+                    <div
+                      className={styles.statLine}
+                      style={
+                        props.stats[0] && {
+                          width: `${props.stats[0].base_stat}px`,
+                        }
+                      }
+                    />
+                  </div>
+                  <div className={styles.stat}>
+                    <div className={styles.statName}>Attack: </div>
+                    <div
+                      className={styles.statLine}
+                      style={
+                        props.stats[1] && {
+                          width: `${props.stats[1].base_stat}px`,
+                        }
+                      }
+                    />
+                  </div>
+                  <div className={styles.stat}>
+                    <div className={styles.statName}>Defense: </div>
+                    <div
+                      className={styles.statLine}
+                      style={
+                        props.stats[2] && {
+                          width: `${props.stats[2].base_stat}px`,
+                        }
+                      }
+                    />
+                  </div>
+                  <div className={styles.stat}>
+                    <div className={styles.statName}>Sp-Attack: </div>
+                    <div
+                      className={styles.statLine}
+                      style={
+                        props.stats[3] && {
+                          width: `${props.stats[3].base_stat}px`,
+                        }
+                      }
+                    />
+                  </div>
+                  <div className={styles.stat}>
+                    <div className={styles.statName}>Sp-Defense: </div>
+                    <div
+                      className={styles.statLine}
+                      style={
+                        props.stats[4] && {
+                          width: `${props.stats[4].base_stat}px`,
+                        }
+                      }
+                    />
+                  </div>
+                  <div className={styles.stat}>
+                    <div className={styles.statName}>Speed: </div>
+                    <div
+                      className={styles.statLine}
+                      style={
+                        props.stats[5] && {
+                          width: `${props.stats[5].base_stat}px`,
+                        }
+                      }
+                    />
+                  </div>
+                </div>
+              </div>
+            </foreignObject>
+          )}
           <path
             className="smallBlackDisplay"
             d="M 590 670 h 200 v 50 h -200 z"
@@ -243,67 +373,69 @@ export default function Pokedex(props) {
               className={styles.input}
               type="text"
               onChange={(e) => props.setSearchTerm(e)}
+              onKeyDown={() => handleKeyDown()}
+              onKeyUp={() => setKeyBoardFill(null)}
               value={props.searchTerm}
               placeholder="PokeSearch"
             />
           </foreignObject>
           <path
             d="M590 420 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 1 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M680 420 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 2 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M770 420 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 3 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M860 420 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 4 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M950 420 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 5 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M590 480 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 6 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M680 480 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 7 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M770 480 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 8 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M860 480 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 9 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
           <path
             d="M950 480 h 90 v 60 h -90 z"
-            fill="lightblue"
+            fill={keyBoardFill === 10 ? "lightblue" : "lightskyblue"}
             stroke="black"
             stroke-width="2"
           />
@@ -319,6 +451,16 @@ export default function Pokedex(props) {
             stroke="black"
             stroke-width="2"
           />
+          <foreignObject x="590" y="580" height="60" width="90">
+            <button className={styles.incDec} onClick={() => props.decrement()}>
+              Prev
+            </button>
+          </foreignObject>
+          <foreignObject x="680" y="580" height="60" width="90">
+            <button className={styles.incDec} onClick={() => props.increment()}>
+              Next
+            </button>
+          </foreignObject>
           <path
             d="M865 560 h 80 v -10 h -80 z"
             fill="rgb(80,80,80)"
@@ -358,22 +500,22 @@ export default function Pokedex(props) {
         <path
           className="pokedexSecondDisplayHighlight"
           d="M660 190 h 40 L 640 390 h -40 z"
-          fill="rgb(255, 255, 255, 0.2)"
+          fill="rgb(255, 255, 255, 0.1)"
         />
         <path
           className="pokedexSecondDisplayHighlight"
           d="M710 190 h 10 L 660 390 h -10 z"
-          fill="rgb(255, 255, 255, 0.2)"
+          fill="rgb(255, 255, 255, 0.1)"
         />
         <path
           className="pokedexSecondDisplayHighlight"
           d="M810 190 h 10 L 760 390 h -10 z"
-          fill="rgb(255, 255, 255, 0.2)"
+          fill="rgb(255, 255, 255, 0.1)"
         />
         <path
           className="pokedexSecondDisplayHighlight"
           d="M890 190 h 40 L 870 390 h -40 z"
-          fill="rgb(255, 255, 255, 0.2)"
+          fill="rgb(255, 255, 255, 0.1)"
         />
         <path
           className="pokedexMainDisplayHighlight"
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,28 @@ export default function Home() {
   const [imageUrl, setImageUrl] = useState("");
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonType, setPokemonType] = useState("");
+  const [pokemonHeight, setPokemonHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [stats, setStats] = useState({});
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [abilities, setAbilities] = useState([]);
+  const [id, setId] = useState(null);
 
-  const handleSearch = () => {
+  const handleSearch = async () => {
+    setLoading(true);
     console.log(searchTerm);
-    fetch(`http://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`)
+    await fetch(`http://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        data.id && setId(data.id);
+        data.abilities && setAbilities(data.abilities);
         data.name && setPokemonName(data.name);
         data.types && setPokemonType(data.types[0].type.name);
+        data.height && setPokemonHeight(data.height);
+        data.weight && setWeight(data.weight);
+        data.stats && setStats(data.stats);
         if (data.id) {
           const strung = data.id.toString();
           console.log(strung.length);
@@ -32,10 +44,87 @@ export default function Home() {
           }
         }
       });
+    setLoading(false);
+  };
+
+  const increment = async () => {
+    setLoading(true);
+    id &&
+      (await fetch(`http://pokeapi.co/api/v2/pokemon/${id + 1}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          data.id && setId(data.id);
+          data.abilities && setAbilities(data.abilities);
+          data.name && setPokemonName(data.name);
+          data.types && setPokemonType(data.types[0].type.name);
+          data.height && setPokemonHeight(data.height);
+          data.weight && setWeight(data.weight);
+          data.stats && setStats(data.stats);
+          if (data.id) {
+            const strung = data.id.toString();
+            console.log(strung.length);
+            if (strung.length === 3) {
+              setImageUrl(data.id);
+            } else if (strung.length === 2) {
+              setImageUrl(`0${strung}`);
+            } else if (strung.length === 1) {
+              console.log(`00${strung}`);
+              setImageUrl(`00${strung}`);
+            } else {
+              return;
+            }
+          }
+        }));
+    setLoading(false);
+  };
+
+  const decrement = async () => {
+    setLoading(true);
+    id !== 1 &&
+      (await fetch(`http://pokeapi.co/api/v2/pokemon/${id - 1}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          data.id && setId(data.id);
+          data.abilities && setAbilities(data.abilities);
+          data.name && setPokemonName(data.name);
+          data.types && setPokemonType(data.types[0].type.name);
+          data.height && setPokemonHeight(data.height);
+          data.weight && setWeight(data.weight);
+          data.stats && setStats(data.stats);
+          if (data.id) {
+            const strung = data.id.toString();
+            console.log(strung.length);
+            if (strung.length === 3) {
+              setImageUrl(data.id);
+            } else if (strung.length === 2) {
+              setImageUrl(`0${strung}`);
+            } else if (strung.length === 1) {
+              console.log(`00${strung}`);
+              setImageUrl(`00${strung}`);
+            } else {
+              return;
+            }
+          }
+        }));
+    setLoading(false);
   };
 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>PokeDex</title>
+        <link
+          rel="icon"
+          href="https://image.pngaaa.com/241/259241-middle.png"
+        />
+      </Head>
+      <img
+        className={styles.background}
+        src="https://free4kwallpapers.com/uploads/originals/2018/06/18/created-a-3d-render-of-a-pokemon-trophy-in-the-grass-wallpaper.jpg"
+        alt=""
+      />
       <Pokedex
         width="100vw"
         height="90vh"
@@ -49,6 +138,14 @@ export default function Home() {
         submit={() => handleSearch()}
         name={pokemonName ? pokemonName : ""}
         type={pokemonType ? pokemonType : ""}
+        stats={stats ? stats : ""}
+        pokemonHeight={pokemonHeight ? pokemonHeight : ""}
+        weight={weight ? weight : ""}
+        loading={loading}
+        id={id}
+        abilities={abilities}
+        increment={() => increment()}
+        decrement={() => decrement()}
       />
     </div>
   );
